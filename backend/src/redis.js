e88829651cf5bce50ea9551672658df376e4e93b
Carryadder Office Laptop <abhishke.miisco@gmail.com>
2026-05-17 23:17:45 +0530

    fix

--- backend/src/redis.js
+++ backend/src/redis.js
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
-const url = process.env.REDIS_URL || 'redis://redis:6379';
+const url = process.env.REDIS_URL || 'redis://localhost:6379';
 
 export const redis = new Redis(url, { lazyConnect: false, maxRetriesPerRequest: null });
 export const subRedis = new Redis(url, { lazyConnect: false, maxRetriesPerRequest: null });
